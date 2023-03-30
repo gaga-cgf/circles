fe4b9076c7d23958cc1d1c8064d590571c5e5dd3
gaga-cgf <a1360851320@qq.com>
2023-03-31 00:34:06 +0800

    revise migrations

--- db/migrate/20230320132754_create_blogs.rb
+++ db/migrate/20230320132754_create_blogs.rb
@@ -6,7 +6,7 @@ class CreateBlogs < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :content
       t.boolean :is_public, default: true
-      t.references :user,foreign_key: true, null: false, index: true
+      t.references :user, foreign_key: true, index: true
       t.timestamps
     end
   end
--- db/migrate/20230322072823_add_users_crypted_password_column.rb
+++ db/migrate/20230322072823_add_users_crypted_password_column.rb
@@ -3,6 +3,6 @@ class AddUsersCryptedPasswordColumn < ActiveRecord::Migration[6.1]
     add_column :users, :crypted_password, :string, null: false
     add_column :users, :salt, :string, null: false
     
-    remove_column :users, :password
+    remove_column :users, :password, :string
   end
 end
